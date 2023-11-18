525b941e1c9bd88982dcfcc22e2e3a56349b6e09
Rhys.k <rhys.k.hm@gmail.com>
2023-11-18 19:26:11 +0800

    Initial Commit

---
+++ toilet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toilet
+//
+//  Created by Rhys.K on 2023/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
